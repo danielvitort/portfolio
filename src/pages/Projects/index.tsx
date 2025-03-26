ed8debd68be5b0a568d492fed30177f94c6daf94
Daniel Vitor da Trindade <121571929+danielvitort@users.noreply.github.com>
2025-03-25 22:07:58 -0300

    adicao de projeto

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,21 +1,70 @@
+"use client"
+
 import { Container } from "@/components/Container"
 import Image from "next/image"
 import Link from "next/link"
 import { IconLogoGithub } from "@/components/Icon"
+import LojaWpp from "@/assets/images/projects/loja-WPP.jpg"
 import AS from "@/assets/images/projects/amigo-secreto.jpg"
 import SistemaFinanceiro from "@/assets/images/projects/sistema-financeiro.jpg"
 import Ecommerce from "@/assets/images/projects/ecommerce.jpg"
 import Chat from "@/assets/images/projects/Chat.jpg"
 import PetShop from "@/assets/images/projects/PetShop.jpg"
+import { motion } from "framer-motion"
 
 const Projects = () => {
     return (
         <section id="projects" className="bg-secondary text-primary flex pb-14">
             <Container>
                 <div>
-                    <h3 className="text-center lg:text-6xl text-4xl font-bold py-14">Projetos</h3>
+                    <h3 className="text-center lg:text-6xl text-4xl font-bold py-16">Projetos</h3>
                     <div className="grid md:grid-cols-2 gap-4">
-                        <div className=" border-2 border-slate-400 rounded-md flex flex-col items-center p-5  gap-4 hover:bg-blue-100 hover:border-primary">
+
+                        <motion.div
+                            className=" border-2 border-slate-400 rounded-md flex flex-col items-center p-5  gap-4 hover:bg-blue-100 hover:border-primary"
+                            initial={{ opacity: 0 }}
+                            whileInView={{ opacity: 1, x: 0 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ duration: 0.5, delay: 0.3 }}
+                        >
+                            <div className="text-2xl font-bold ">Venda de Comida Japonesa via WhatsApp </div>
+                            <Image className="lg:h-80" src={LojaWpp} alt="Projeto amigo secreto" width={450} quality={100} />
+                            <div className="flex flex-col">
+                                <div >Uma plataforma intuitiva e responsiva para a venda de comida japonesa, proporcionando uma experiência de compra fluida e eficiente.
+                                    O usuário pode navegar pelo catálogo, adicionar itens ao pedido e finalizar a compra diretamente pelo WhatsApp.
+                                </div>
+                                <div className="flex pt-2">
+                                    <span className="font-bold">Tecnologias:
+                                        NextJS, TypeScript, TailwindCSS, Shadcn/ui, Zod.</span>
+                                </div>
+                            </div>
+                            <motion.div
+                                className="flex gap-2 items-center mt-2"
+                                initial={{ opacity: 0, y: 50 }}
+                                whileInView={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0, y: 50 }}
+                                transition={{ duration: 0.5, delay: 0.2 }}
+                            >
+                                <IconLogoGithub width="2.5em" height="2.5em" />
+                                <span>Repositório:</span>
+                                <Link
+                                    href="https://github.com/danielvitort/Loja-WPP"
+                                    className="flex items-center border-2 border-slate-400 text-primary font-bold px-5 py-1 rounded-3xl hover:border-primary hover:bg-sky-950 hover:text-white"
+                                    target="_banq"
+                                >
+                                    <span>Front End</span>
+                                </Link>
+                            </motion.div>
+                        </motion.div>
+
+
+                        <motion.div
+                            className=" border-2 border-slate-400 rounded-md flex flex-col items-center p-5  gap-4 hover:bg-blue-100 hover:border-primary"
+                            initial={{ opacity: 0 }}
+                            whileInView={{ opacity: 1, x: 0 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ duration: 0.5, delay: 0.3 }}
+                        >
                             <div className="text-2xl font-bold">Amigo Secreto/Oculto</div>
                             <Image className="lg:h-80" src={AS} alt="Projeto amigo secreto" width={450} quality={100} />
                             <div className="flex flex-col">
@@ -31,7 +80,13 @@ const Projects = () => {
                                         Prisma, Zod.</span>
                                 </div>
                             </div>
-                            <div className="flex gap-2 items-center mt-2">
+                            <motion.div
+                                className="flex gap-2 items-center mt-2"
+                                initial={{ opacity: 0, y: 50 }}
+                                whileInView={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0, y: 50 }}
+                                transition={{ duration: 0.5, delay: 0.2 }}
+                            >
                                 <IconLogoGithub width="2.5em" height="2.5em" />
                                 <span>Repositórios:</span>
                                 <div className="flex flex-col lg:flex-row gap-2">
@@ -50,10 +105,16 @@ const Projects = () => {
                                         <span >Back End</span>
                                     </Link>
                                 </div>
-                            </div>
-                        </div>
+                            </motion.div>
+                        </motion.div>
 
-                        <div className=" border-2 border-slate-400 rounded-md flex flex-col items-center p-5  gap-4 hover:bg-blue-100 hover:border-primary">
+                        <motion.div
+                            className=" border-2 border-slate-400 rounded-md flex flex-col items-center p-5  gap-4 hover:bg-blue-100 hover:border-primary"
+                            initial={{ opacity: 0 }}
+                            whileInView={{ opacity: 1, x: 0 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ duration: 0.5, delay: 0.3 }}
+                        >
                             <div className="text-2xl font-bold ">Sistema Financeiro</div>
                             <Image className="lg:h-80" src={SistemaFinanceiro} alt="Projeto amigo secreto" width={450} quality={100} />
                             <div className="flex flex-col">
@@ -67,8 +128,13 @@ const Projects = () => {
                                         ReactJS, TypeScript, Styled-Components.</span>
                                 </div>
                             </div>
-                            <div className="flex gap-2 items-center mt-2">
-                                <IconLogoGithub width="2.5em" height="2.5em" />
+                            <motion.div
+                                className="flex gap-2 items-center mt-2"
+                                initial={{ opacity: 0, y: 50 }}
+                                whileInView={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0, y: 50 }}
+                                transition={{ duration: 0.5, delay: 0.2 }}
+                            >                                <IconLogoGithub width="2.5em" height="2.5em" />
                                 <span>Repositório:</span>
                                 <Link
                                     href="https://github.com/danielvitort/react-sistema-financeiro"
@@ -77,10 +143,16 @@ const Projects = () => {
                                 >
                                     <span>Front End</span>
                                 </Link>
-                            </div>
-                        </div>
+                            </motion.div>
+                        </motion.div>
 
-                        <div className=" border-2 border-slate-400 rounded-md flex flex-col items-center p-5  gap-4 hover:bg-blue-100 hover:border-primary">
+                        <motion.div
+                            className=" border-2 border-slate-400 rounded-md flex flex-col items-center p-5  gap-4 hover:bg-blue-100 hover:border-primary"
+                            initial={{ opacity: 0 }}
+                            whileInView={{ opacity: 1, x: 0 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ duration: 0.5, delay: 0.3 }}
+                        >
                             <div className="text-2xl font-bold ">E-Commerce</div>
                             <Image className="lg:h-80" src={Ecommerce} alt="Projeto amigo secreto" width={450} quality={100} />
                             <div className="flex flex-col">
@@ -93,7 +165,13 @@ const Projects = () => {
                                         ReactJS, JavaScript, Styled-Components.</span>
                                 </div>
                             </div>
-                            <div className="flex gap-2 items-center mt-2">
+                            <motion.div
+                                className="flex gap-2 items-center mt-2"
+                                initial={{ opacity: 0, y: 50 }}
+                                whileInView={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0, y: 50 }}
+                                transition={{ duration: 0.5, delay: 0.2 }}
+                            >
                                 <IconLogoGithub width="2.5em" height="2.5em" />
                                 <span>Repositório:</span>
                                 <Link
@@ -103,10 +181,16 @@ const Projects = () => {
                                 >
                                     <span>Front End</span>
                                 </Link>
-                            </div>
-                        </div>
+                            </motion.div>
+                        </motion.div>
 
-                        <div className=" border-2 border-slate-400 rounded-md flex flex-col items-center p-5  gap-4 hover:bg-blue-100 hover:border-primary">
+                        <motion.div
+                            className=" border-2 border-slate-400 rounded-md flex flex-col items-center p-5  gap-4 hover:bg-blue-100 hover:border-primary"
+                            initial={{ opacity: 0 }}
+                            whileInView={{ opacity: 1, x: 0 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ duration: 0.5, delay: 0.3 }}
+                        >
                             <div className="text-2xl font-bold ">Chat com Socket.IO</div>
                             <Image className="lg:h-80" src={Chat} alt="Projeto amigo secreto" width={450} quality={100} />
                             <div className="flex flex-col">
@@ -120,8 +204,13 @@ const Projects = () => {
                                         NodeJS, Express, Socket.IO.</span>
                                 </div>
                             </div>
-                            <div className="flex gap-2 items-center mt-2">
-                                <IconLogoGithub width="2.5em" height="2.5em" />
+                            <motion.div
+                                className="flex gap-2 items-center mt-2"
+                                initial={{ opacity: 0, y: 50 }}
+                                whileInView={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0, y: 50 }}
+                                transition={{ duration: 0.5, delay: 0.2 }}
+                            >                                <IconLogoGithub width="2.5em" height="2.5em" />
                                 <span>Repositório:</span>
                                 <Link
                                     href="https://github.com/danielvitort/node-chat-socket"
@@ -130,10 +219,16 @@ const Projects = () => {
                                 >
                                     <span>Back End</span>
                                 </Link>
-                            </div>
-                        </div>
+                            </motion.div>
+                        </motion.div>
 
-                        <div className=" border-2 border-slate-400 rounded-md flex flex-col items-center p-5  gap-4 hover:bg-blue-100 hover:border-primary">
+                        <motion.div
+                            className=" border-2 border-slate-400 rounded-md flex flex-col items-center p-5  gap-4 hover:bg-blue-100 hover:border-primary"
+                            initial={{ opacity: 0 }}
+                            whileInView={{ opacity: 1, x: 0 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ duration: 0.5, delay: 0.3 }}
+                        >
                             <div className="text-2xl font-bold ">PetShop</div>
                             <Image className="lg:h-80" src={PetShop} alt="Projeto amigo secreto" width={450} quality={100} />
                             <div className="flex flex-col">
@@ -146,7 +241,13 @@ const Projects = () => {
                                         NodeJS, Express, TypeScript.</span>
                                 </div>
                             </div>
-                            <div className="flex gap-2 items-center mt-2">
+                            <motion.div
+                                className="flex gap-2 items-center mt-2"
+                                initial={{ opacity: 0, y: 50 }}
+                                whileInView={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0, y: 50 }}
+                                transition={{ duration: 0.5, delay: 0.2 }}
+                            >
                                 <IconLogoGithub width="2.5em" height="2.5em" />
                                 <span>Repositório:</span>
                                 <Link
@@ -156,8 +257,8 @@ const Projects = () => {
                                 >
                                     <span>Back End</span>
                                 </Link>
-                            </div>
-                        </div>
+                            </motion.div>
+                        </motion.div>
                     </div>
                 </div>
             </Container>
